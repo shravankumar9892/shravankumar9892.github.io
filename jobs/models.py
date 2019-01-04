c062f51f2fe41e7a484e16fd49f2a740e8fe7775
shravankumar9892 <shravankumarshetty9892@gmail.com>
2019-01-04 14:07:09 +0530

    project description added

--- jobs/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# Models should be created with python class.
-class Job(models.Model):
-    image = models.ImageField(upload_to = "images/")
-    summary = models.CharField(max_length = 100)
